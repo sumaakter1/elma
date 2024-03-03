1cea0ed8558cf02a5e31bebd7a2f65ebd6fe1dec
Suma Akter <sumaakter9247@gmail.com>
2024-03-04 00:14:55 +0600

    testimonial added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-//  header scroll 
-// let nav = document.querySelector(".navbar");
-// window.onscroll = function(){
-//     if(document.documentElement.scrolled > 20){
-//         nav.classList.add("header-scrolled");
-//     }else{
-//         nav.classList.remove("header-scrolled")
-//         }
-//     }
 // nav hide 
 let navBar = document.querySelectorAll(".navbar-nav .nav-link");
 let navCollapse = document.querySelector(".navbar-collapse");
@@ -17,4 +8,3 @@ navBar.forEach(function(a){
 
 
 })
-// portfolio 
